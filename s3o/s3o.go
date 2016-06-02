a1bdd75add11703e066301bc6a2f7eed885c186f
Martin Garton <martin.garton@ft.com>
2016-06-02 21:53:56 +0100

    periodic re-fetch of public key

--- s3o/s3o.go
+++ s3o/s3o.go
@@ -7,19 +7,48 @@ import (
 	"crypto/sha1"
 	"crypto/x509"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
 
-var pubKey *rsa.PublicKey
+var (
+	lk     sync.RWMutex
+	pubKey *rsa.PublicKey
+	period = time.Duration(5 * time.Minute)
+)
 
 func init() {
+	go periodicFetchKey()
+}
+
+func periodicFetchKey() {
+	for {
+		newPubKey, err := fetchPubkey()
+		if err != nil {
+			log.Printf("failed to fetch s3o public key: %s\n", err.Error())
+		} else {
+			lk.Lock()
+			pubKey = newPubKey
+			lk.Unlock()
+		}
+		lk.RLock()
+		p := period
+		lk.RUnlock()
+		time.Sleep(p)
+	}
+}
+
+func fetchPubkey() (*rsa.PublicKey, error) {
 	resp, err := http.Get("https://s3o.ft.com/publickey")
 	if err != nil || resp.StatusCode != http.StatusOK {
-		panic("failed to read s3o public key")
+		return nil, errors.New("failed to read s3o public key")
 	}
 	defer func() {
 		_, _ = io.Copy(ioutil.Discard, resp.Body)
@@ -27,19 +56,19 @@ func init() {
 	}()
 	var buf bytes.Buffer
 	if _, err := io.Copy(&buf, resp.Body); err != nil {
-		panic("failed to read s3o public key")
+		return nil, errors.New("failed to read s3o public key")
 	}
 	dec := make([]byte, 8192) // should be enough for a while.
 	i, err := base64.StdEncoding.Decode(dec, buf.Bytes())
 	if err != nil {
-		panic("failed to base64 decode s3o public key")
+		return nil, errors.New("failed to base64 decode s3o public key")
 	}
 
 	pub, err := x509.ParsePKIXPublicKey(dec[0:i])
 	if err != nil {
-		panic("failed to parse s3o public key")
+		return nil, errors.New("failed to parse s3o public key")
 	}
-	pubKey = pub.(*rsa.PublicKey)
+	return pub.(*rsa.PublicKey), nil
 }
 
 // Handler wraps the given handler in s3o authentication
@@ -81,6 +110,14 @@ func Handler(next http.Handler) http.Handler {
 			return
 		}
 
+		lk.RLock()
+		defer lk.RUnlock()
+
+		if pubKey == nil {
+			fmt.Fprint(w, "public s3o key unavailable")
+			w.WriteHeader(http.StatusForbidden)
+		}
+
 		if err := rsa.VerifyPKCS1v15(pubKey, crypto.SHA1, hash.Sum(nil), sig); err != nil {
 			fmt.Fprint(w, "failed to authenticate")
 			w.WriteHeader(http.StatusForbidden)
@@ -89,4 +126,11 @@ func Handler(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+// SetKeyFetchPeriod changes how often we fetch the s3o public key. The default is 5 minutes.
+func SetKeyFetchPeriod(d time.Duration) {
+	lk.Lock()
+	defer lk.Unlock()
+	period = d
 }
